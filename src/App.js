0398e77c2b674866b8da23e8b67ee1afe12c1a41
MarcosHome <m.andrade.assis@gmail.com>
2023-11-23 22:58:23 -0300

    ddados view

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import './App.css';
 import Template from './Template';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './Routes';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Template />
-//       <Routes></Routes>
-//     </BrowserRouter>
-//   );
-// }
 
 function App() {
     return (
