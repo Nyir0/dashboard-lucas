fbd02d004e00a6b4232106846b41aabbdafb310a
Gabriel Teofilo <gabriel.ol.teo@outlook.com>
2023-11-25 11:49:36 -0300

    ajuste do padding da main

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,7 @@ import { Hello } from './components/titlePage';
 function  Main(){
 
     return(
-        <main className='pl-[22vw] pt-[12vw]'>
+        <main className='pl-[22vw] pt-[12vh]'>
             <Hello/>                
         </main>
     );
