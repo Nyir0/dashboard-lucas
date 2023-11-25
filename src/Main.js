8a7fce8ab1c3f8382412f031c3c4fe7001474a6d
Gabriel Teofilo <gabriel.ol.teo@outlook.com>
2023-11-25 20:59:19 -0300

    atualizacao

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 
 import { Hello } from './components/titlePage';
 import TryNow from './components/TryNow';
+import Dounts from './components/chartsDounts';
 
 function  Main(){
 
@@ -8,6 +9,7 @@ function  Main(){
         <main className='pl-[22vw] pt-[12vh]'>
             <Hello/>  
             <TryNow />              
+            <Dounts/>
         </main>
     );
 }
---
+++ src/components/chartDoughnut.js
@@ -0,0 +1,55 @@
+import $ from 'jquery';
+import React from 'react';
+import Chart from 'chart.js/auto';
+import 'chartjs-plugin-datalabels';
+
+$(document).ready(function() {
+    var ctx = document.getElementById("myChart");
+
+    const Options = {
+        cutout: '75%',
+        responsive: false,
+        maintainAspectRatio: false,
+        onHover: function (event, legendItem) {
+            var legend = this.legend.active;
+            console.log(this.legend);
+            // if (event.type === 'mousemove') {
+            //   legend.active = true;
+            // } else if (event.type === 'mouseout') {
+            //   legend.active = false;
+            // }
+            // this.update(); // Atualiza o gráfico para refletir as alterações
+        },
+        plugins: {
+            datalabels: {
+                display: false,
+            },
+            tooltip: {
+                enabled: false,
+            },
+            legend: {
+                display: false, // Configuração para esconder as legendas
+            },
+        }
+    }
+
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+            datasets: [{
+            label: "",
+            data: [25, 65, 15, 10],
+            backgroundColor: [
+                'rgb(152 159 224',
+                'rgb(103 192 135',
+                'rgb(239 214 84',
+                'rgb(221 140 140'
+            ],
+            borderWidth: 0.2,
+            cutoutPercentage: 80
+            }]
+        },
+        options: Options
+    });
+})
